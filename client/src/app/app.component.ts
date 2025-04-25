84136331d644c8becb223eefc28fe13fab0f0862
nalbmor1 <nadiaalmoral@hotmail.com>
2025-04-25 12:00:19 +0200

    cosas

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../core/header/header.component';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet, HeaderComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'client';
+    title = 'Tutorial de Angular';
 }
