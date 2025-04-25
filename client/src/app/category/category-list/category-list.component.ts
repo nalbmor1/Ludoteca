2154fd694be0913f0c44d2e92188ff0d87318508
nalbmor1 <nadiaalmoral@hotmail.com>
2025-04-25 12:27:30 +0200

    boton nueva categoria

--- client/src/app/category/category-list/category-list.component.ts
+++ client/src/app/category/category-list/category-list.component.ts
@@ -5,6 +5,9 @@ import { CommonModule } from '@angular/common';
 import { MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { CategoryService } from '../category.service';
+import { MatDialog } from '@angular/material/dialog';
+import { CategoryEditComponent } from '../category-edit/category-edit.component';
 
 @Component({
     selector: 'app-category-list',
@@ -19,12 +22,27 @@ import { MatButtonModule } from '@angular/material/button';
     styleUrl: './category-list.component.scss'
 })
 export class CategoryListComponent implements OnInit{
-
     dataSource = new MatTableDataSource<Category>();
     displayedColumns: string[] = ['id', 'name', 'action'];
 
-    constructor() { }
+    constructor(
+        private categoryService: CategoryService,
+        public dialog: MatDialog,
+    ) { }
 
     ngOnInit(): void {
+        this.categoryService.getCategories().subscribe(
+            categories => this.dataSource.data = categories
+        );
     }
+
+    createCategory() {    
+      const dialogRef = this.dialog.open(CategoryEditComponent, {
+        data: {}
+      });
+  
+      dialogRef.afterClosed().subscribe(result => {
+        this.ngOnInit();
+      });    
+    } 
 }
