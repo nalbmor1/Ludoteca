bdb061fb31ae83357dc22d3fcae54e45639d54cd
nalbmor1 <nadiaalmoral@hotmail.com>
2025-04-29 10:35:28 +0200

    lista juegos

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+    { path: '', redirectTo: '/games', pathMatch: 'full'},
     { path: 'categories', loadComponent: () => import('./category/category-list/category-list.component').then(m => m.CategoryListComponent)},
     { path: 'authors', loadComponent: () => import('./author/author-list/author-list.component').then(m => m.AuthorListComponent)},
+    { path: 'games', loadComponent: () => import('./game/game-list/game-list.component').then(m => m.GameListComponent)}
 ];
