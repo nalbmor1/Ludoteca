384a44b2de3d7902106b74a1c85a34dbdd474a1e
nalbmor1 <nadiaalmoral@hotmail.com>
2025-04-30 12:36:25 +0200

    editar prestamo

--- client/src/app/rent/rent-edit/rent-edit.component.ts
+++ client/src/app/rent/rent-edit/rent-edit.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { RentService } from '../rent.service';
+import { Rent } from '../model/Rent';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_NATIVE_DATE_FORMATS, MatNativeDateModule, NativeDateAdapter } from '@angular/material/core';
 
 @Component({
-  selector: 'app-rent-edit',
-  imports: [],
-  templateUrl: './rent-edit.component.html',
-  styleUrl: './rent-edit.component.scss'
+    selector: 'app-rent-edit',
+    standalone: true,
+    imports: [FormsModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatDatepickerModule, MatNativeDateModule],
+    templateUrl: './rent-edit.component.html',
+    styleUrl: './rent-edit.component.scss',
+    providers: [ {provide: DateAdapter, useClass: NativeDateAdapter}, {provide: MAT_DATE_FORMATS, useValue: MAT_NATIVE_DATE_FORMATS}, ],
 })
-export class RentEditComponent {
+export class RentEditComponent implements OnInit {
+    rent: Rent;
 
+    constructor(
+        public dialogRef: MatDialogRef<RentEditComponent>,
+        @Inject(MAT_DIALOG_DATA) public data: any,
+        private rentService: RentService
+    ) {}
+
+    ngOnInit(): void {
+        this.rent = this.data.rent ? Object.assign({}, this.data.rent) : new Rent();
+    }
+
+    onSave() {
+        this.rentService.saveRent(this.rent).subscribe(() => {
+            this.dialogRef.close();
+        });
+    }
+
+    onClose() {
+        this.dialogRef.close();
+    }
 }
--- client/src/app/rent/rent.service.ts
+++ client/src/app/rent/rent.service.ts
@@ -4,22 +4,27 @@ import { Pageable } from '../core/model/page/Pageable';
 import { Rent } from './model/Rent';
 import { RentPage } from './model/RentPage';
 import { RENT_DATA } from './model/mock-rent';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
     providedIn: 'root',
 })
 export class RentService {
-    constructor() {}
+    constructor(private http: HttpClient) {}
+
+    private baseUrl = 'http://localhost:8080/rent';
 
     getRents(pageable: Pageable): Observable<RentPage> {
-        return of(RENT_DATA);
+        return this.http.post<RentPage>(this.baseUrl, { pageable: pageable });
     }
 
-    saveRent(rent: Rent): Observable<void> {
-        return of(null);
+    saveRent(rent: Rent): Observable<Rent> {
+        const { id } = rent;
+        const url = id ? `${this.baseUrl}/${id}` : this.baseUrl;
+        return this.http.put<Rent>(url, rent);
     }
 
     deleteRent(idRent: number): Observable<void> {
-        return of(null);
+        return this.http.delete<void>(`${this.baseUrl}/${idRent}`);
     }
 }
